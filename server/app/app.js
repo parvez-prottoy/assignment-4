96b77759ca60ec99cdf169af6c5a061661ec54c2
parvezAhmedWEB <parvezahmedprottoy.web@gmail.com>
2024-04-22 13:34:41 +0600

    added global error handler

--- server/app/app.js
+++ server/app/app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
+const { notFoundHandler, errorHandler } = require("../src/utility/error/error");
 
 // global middleware
 app.use(require("../src/middlewares/global.middleware"));
+// global error handler
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 module.exports = app;
