ffac5ad2791dcc8d4a84939f8ff46a42200bea51
parvezAhmedWEB <parvezahmedprottoy.web@gmail.com>
2024-04-22 13:51:49 +0600

    added base route and global react frontAnd routing

--- server/app/app.js
+++ server/app/app.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const app = express();
 const { notFoundHandler, errorHandler } = require("../src/utility/error/error");
+const routes = require("../src/routes/index.route");
 
 // global middleware
 app.use(require("../src/middlewares/global.middleware"));
+// routers
+app.set("etag", false);
+app.use(routes);
+app.use(express.static("client/dist"));
 // global error handler
 app.use(notFoundHandler);
 app.use(errorHandler);
+
+// Add React Front End Routing
+app.get("*", function (req, res) {
+  res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+});
 
 module.exports = app;
