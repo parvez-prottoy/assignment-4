549d372725756fb3aa6b43b45bde84c62d819e2a
parvezAhmedWEB <parvezahmedprottoy.web@gmail.com>
2024-04-22 13:20:53 +0600

    added connected mongoDB

--- server/config/config.js
+++ server/config/config.js
@@ -2,5 +2,8 @@ const dev = {
   app: {
     port: 4040,
   },
+  db: {
+    url: "mongodb://localhost:27017/CRUR-Food",
+  },
 };
 module.exports = dev;
--- server/index.js
+++ server/index.js
@@ -3,9 +3,11 @@ const app = require("./app/app");
 const PORT = require("./config/config").app.port;
 const chalk = require("chalk");
 const server = http.createServer(app);
+const connectDB = require("./config/db");
 
-server.listen(PORT, () => {
+server.listen(PORT, async () => {
   console.log(
     chalk.black.bgBlueBright(`server is running at http://localhost:${PORT}`)
   );
+  await connectDB();
 });
