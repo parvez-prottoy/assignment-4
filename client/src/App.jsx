043548fcb88017bf68b842fb2bf99bebd749500d
parvezAhmedWEB <parvezahmedprottoy.web@gmail.com>
2024-04-23 16:57:23 +0600

    added new tailwind css configaration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,9 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import React from "react";
+
+const App = () => {
+  return (
+    <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
+  );
+};
+
+export default App;
