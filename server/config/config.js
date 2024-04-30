6a4a6747885ec3afd5fac6276fb33e486cf49269
parvezAhmedWEB <parvezahmedprottoy.web@gmail.com>
2024-04-30 14:07:27 +0600

    added mongodb atlas data base url

--- server/config/config.js
+++ server/config/config.js
@@ -3,7 +3,7 @@ const dev = {
     port: 4040,
   },
   db: {
-    url: "mongodb://localhost:27017/CRUR-Food",
+    url: "mongodb+srv://practices:parvez4040@cluster0.gxg0obw.mongodb.net/CRUD-Foods",
   },
 };
 module.exports = dev;
