17656e2a8128e987b074412ba79a71d60bcf243a
parvezAhmedWEB <parvezahmedprottoy.web@gmail.com>
2024-05-04 14:42:37 +0600

    addeed base url

--- client/src/utility/APICalling.js
+++ client/src/utility/APICalling.js
@@ -1,2 +1,2 @@
 export const baseUrl =
-  "https://assignment-4-hixhh6syj-parvez-ahmed-prottoys-projects.vercel.app/api/v1";
+  "https://assignment-4-oav7w89gv-parvez-ahmed-prottoys-projects.vercel.app/api/v1";
